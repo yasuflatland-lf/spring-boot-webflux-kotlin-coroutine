7efa609c16948991a2a4df836736ed1f12708ac2
Yasuyuki Takeo <takeo@nstock.co.jp>
2025-02-01 11:20:00 +0900

    Upgrade Spring to 3.4.2 and JDK21 compatible

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Container in which to build the application
-FROM gradle:8.1.1-jdk11-alpine as builder
+FROM gradle:8.12-jdk21-alpine as builder
 
 # Copy the source code into the builder container
 WORKDIR /app
@@ -9,7 +9,7 @@ COPY . .
 RUN gradle assemble
 
 # Container in which to run the application
-FROM eclipse-temurin:17.0.10_7-jdk-alpine
+FROM eclipse-temurin:21-jdk-ubi9-minimal
 
 # Copy the jar from the builder container into the run container
 COPY --from=builder /app/build/libs/spring-boot-webflux-kotlin-coroutine-*.jar spring-boot-webflux-kotlin-coroutine.jar
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,27 +3,27 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     java
     jacoco
-	id("org.springframework.boot") version "3.2.2"
-	id("io.spring.dependency-management") version "1.1.4"
-    id("org.springdoc.openapi-gradle-plugin") version "1.8.0"
-    id("org.openapi.generator") version "7.2.0"
+	id("org.springframework.boot") version "3.4.2"
+	id("io.spring.dependency-management") version "1.1.7"
+    id("org.springdoc.openapi-gradle-plugin") version "1.9.0"
+    id("org.openapi.generator") version "7.10.0"
 
-	kotlin("jvm") version "1.9.22"
-	kotlin("plugin.spring") version "1.9.22"
+    kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
 }
 
 group = "com.sennproject"
 version = "1.0.0"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_21
 
 repositories {
     mavenCentral()
 }
 
-extra["kotestVersion"] = "5.8.0"
-extra["openAPIVersion"] = "1.7.0"
-extra["testcontainersVersion"] = "1.19.4"
-extra["coroutinesCoreVersion"] = "1.8.0-RC2"
+extra["kotestVersion"] = "5.9.1"
+extra["openAPIVersion"] = "1.8.0"
+extra["testcontainersVersion"] = "1.20.4"
+extra["coroutinesCoreVersion"] = "1.10.1"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
@@ -48,8 +48,9 @@ dependencies {
     implementation("org.springdoc:springdoc-openapi-webflux-ui:${property("openAPIVersion")}")
 
     // Database
-    implementation("io.asyncer:r2dbc-mysql:1.0.6")
-    runtimeOnly("com.mysql:mysql-connector-j:8.0.+")
+    implementation("io.asyncer:r2dbc-mysql:1.3.1")
+    implementation("io.r2dbc:r2dbc-pool:1.0.2.RELEASE")
+    runtimeOnly("com.mysql:mysql-connector-j:8.4.+")
     implementation("org.springframework.data:spring-data-commons")
     implementation("org.springframework.data:spring-data-relational")
 
@@ -63,13 +64,18 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
     testImplementation("io.projectreactor:reactor-test")
-    testImplementation("org.testcontainers:junit-jupiter")
-    testImplementation("org.testcontainers:mysql")
+    testImplementation("org.testcontainers:junit-jupiter:1.17.3")
+    testImplementation("org.testcontainers:mysql:1.17.3")
     testImplementation("org.testcontainers:r2dbc")
 
     // Faker
     implementation("net.datafaker:datafaker:1.8.1")
 
+    // Netty native libraries for MacOS
+    if (System.getProperty("os.name").lowercase().contains("mac")) {
+        implementation("io.netty:netty-resolver-dns-native-macos:4.1.68.Final:osx-aarch_64")
+        implementation("io.netty:netty-resolver-dns-native-macos:4.1.68.Final:osx-x86_64")
+    }
 }
 
 dependencyManagement {
@@ -82,7 +88,7 @@ dependencyManagement {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
 }
 
